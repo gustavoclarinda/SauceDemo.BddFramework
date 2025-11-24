c5c0bef74b437dae84b1d403d4e8e4c0a84f947c
Gustavo Clarinda <gd.clarinda@gmail.com>
2025-11-24 14:48:42 +0000

    Ensure Allure lifecycle initializes

---
+++ Hooks/AllureHooks.cs
@@ -0,0 +1,32 @@
+using System;
+using System.IO;
+using Allure.Commons;
+using TechTalk.SpecFlow;
+
+namespace SauceDemo.BddFramework.Hooks
+{
+    [Binding]
+    public class AllureHooks
+    {
+        [BeforeTestRun(Order = -2000)]
+        public static void EnsureAllureDirectories()
+        {
+            var configPath = Path.Combine(AppContext.BaseDirectory, "allureConfig.json");
+            if (!File.Exists(configPath))
+            {
+                var fallbackConfig = Path.Combine(Directory.GetCurrentDirectory(), "allureConfig.json");
+                if (File.Exists(fallbackConfig))
+                {
+                    configPath = fallbackConfig;
+                }
+            }
+
+            Environment.SetEnvironmentVariable("ALLURE_CONFIG", configPath);
+
+            var resultsDirectory = Path.Combine(Directory.GetCurrentDirectory(), "TestReports", "allure-results");
+            Directory.CreateDirectory(resultsDirectory);
+
+            AllureLifecycle.SetInstance(new AllureLifecycle());
+        }
+    }
+}
